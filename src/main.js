25955d809f00098c0b7ae21b9f4c27116d5daf43
LY0528 <lyyan584521@163.com>
2017-06-10 16:24:02 +0800

    增加路由

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import App from './App'
 import { Header, Swipe, SwipeItem } from 'mint-ui'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.use(VueRouter)
 import './common/stylus/index.styl'
-
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+import homePage from './components/homePage/homePage'
+import psychology from './components/psychology/psychology'
+var routes = [
+	{path:'/homePage',component:homePage},
+	{path:'/psychology',component:psychology}
+]
+var router = new VueRouter({
+	routes
 })
+var app = new Vue({
+	router,
+	render: h => h(App)
+})
+app.$mount('#app')
+router.push('/homePage')
